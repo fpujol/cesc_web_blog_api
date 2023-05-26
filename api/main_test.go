920f7d71a6b45c5d1ff5c9c3a066a11bc455ff62
Francesc Pujol <fpbosch@gmail.com>
2023-05-26 21:43:54 +0200

    Changed test

--- api/main_test.go
+++ api/main_test.go
@@ -11,17 +11,19 @@ import (
 	"blogapi/util"
 
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/require"
 )
 
 func newTestServer(t *testing.T, store db.Store) *Server {
+	log := logrus.New()
 	ctx := context.Background()
 	config := util.Config{
 		TokenSymmetricKey:   utils.RandomString(32),
 		AccessTokenDuration: time.Minute,
 	}
 
-	server, err := NewServer(ctx, config, store)
+	server, err := NewServer(log, ctx, config, store)
 	require.NoError(t, err)
 
 	return server
